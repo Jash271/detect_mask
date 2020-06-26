5cd07d0e8b67de36aca2ecbec6e30063d56c2895
Jash271 <51506649+Jash271@users.noreply.github.com>
2020-06-26 15:18:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ setuptools.setup(
     name='detect_mask',
     license="MIT",
     description='Python Package for detecting masks in the input Photos ,Pull out the camera for real time mask detection',
-    version='v0.3.0',
+    version='v0.4.0',
     long_description=description,
     url='https://github.com/Jash271/Detect_Mask',
     packages=setuptools.find_packages(),
     
     python_requires=">=3.6.8",
-    install_requires=[],
+    install_requires=['opencv-python'],
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
